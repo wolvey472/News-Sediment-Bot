92d4eee316111edda8a34b7ba191c6b50fb53671
Kory Shae <kory.shae@border.bank>
2026-06-29 12:45:04 -0500

    this works ig

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ from datetime import datetime
 #--------------
 
 #Machine Learning
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"   # hide INFO/WARNING/ERROR C++ logs
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"  # hide the oneDNN message
 import tensorflow as tf
 
 if tf.__version__:
-    ...
+    print("woprks")
 else:
     print("NOT WORKING")
