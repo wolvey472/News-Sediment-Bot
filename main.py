0fb296495b0ee6487d42dd34bb65a58a70a05671
Kory Shae <kory.shae@border.bank>
2026-06-29 12:39:35 -0500

    Add main file

---
+++ main.py
@@ -0,0 +1,17 @@
+#Web scraping
+import requests as rq
+import feedparser
+#--------------
+
+#Time
+import time as t
+from datetime import datetime
+#--------------
+
+#Machine Learning
+import tensorflow as tf
+
+if tf.__version__:
+    ...
+else:
+    print("NOT WORKING")
